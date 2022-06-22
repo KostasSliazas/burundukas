aeacc6887a236b1944c9bff382469884dcff6202
Kostas <kostas.sliazas@gmail.com>
2022-06-22 20:59:01 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,120 +1,39 @@
-(function () {
-  'use strict'
-  const debounce = function debounce (fn, delay) {
-    let timoutID
-    let args
-    return function () {
-      for (let _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) args[_key] = arguments[_key]
-      timoutID && clearTimeout(timoutID)
-      timoutID = setTimeout(function () {
-        fn.apply(undefined, args)
-      }, delay)
-    }
-  }
+(function ($) {
+  const ext = document.createElement('script')
+  ext.setAttribute('defer', 'defer')
+  ext.setAttribute('src', './js/gall7.min.js')
+  document.head.appendChild(ext)
+  $('#kmenu > ul.nav.navbar-nav.navbar-right').append(
+    '<a data-toggle="collapse" data-target=".navbar-collapse.in" style="background:#3b5998;color:#eee; border-radius:5px; line-height:50px; margin:0 24px; padding:0 6px;" href="https://www.facebook.com/sharer/sharer.php?u=https://kostassliazas.github.io/burundukas.github.io/" rel="nofollow" target="_blank">Share</a>'
+  )
 
-  // const boxes = document.querySelectorAll('.box')
-  // const observer = new window.IntersectionObserver((entries, observer) => {
-  //   entries.forEach(entry => {
-  //     console.log('300')
-  //     const er = entry
-  //     debounce(e=>er.target.style.display = 'block', 3000)(e)
-
-  //     // entry.target.removeAttribute('loading')
-  //   })
-  // })
-  // boxes.forEach(box => observer.observe(box))
-
-  function ready (fn) {
-    document.readyState !== 'loading'
-      ? fn()
-      : document.addEventListener
-        ? document.addEventListener('DOMContentLoaded', fn)
-        : document.attachEvent('onreadystatechange', function () {
-          document.readyState !== 'loading' && fn()
-        })
-  }
-  ready(function () {
-    // const resource = document.createElement('link')
-    // resource.setAttribute('rel', 'stylesheet')
-    // resource.setAttribute('href', 'https://fonts.googleapis.com/css?family=K2D:400,400i&amp;subset=latin-ext&display=swap')
-    // resource.setAttribute('type', 'text/css')
-    // document.getElementsByTagName('head')[0].appendChild(resource)
-
-    const link = document.querySelector("link[rel='canonical']") ? document.querySelector("link[rel='canonical']") : document.createElement('link')
-    link.setAttribute('rel', 'canonical')
-    link.setAttribute('href', location.protocol + '//' + location.host + location.pathname)
-    document.head.appendChild(link)
-
-    const ext1 = document.createElement('script')
-    ext1.setAttribute('src', './js/appinstall.min.js')
-    ext1.setAttribute('defer', 'defer')
-    document.head.appendChild(ext1)
-
-    const ext2 = document.createElement('script')
-    ext2.setAttribute('src', './js/imgs.min.js')
-    ext2.setAttribute('defer', 'defer')
-    document.head.appendChild(ext2)
-
-    const ext3 = document.createElement('script')
-    ext3.setAttribute('src', './js/gall7.min.js')
-    ext3.setAttribute('defer', 'defer')
-    document.head.appendChild(ext3)
-
-    const col1 = document.getElementById('leftcolumn')
-    const col2 = document.getElementById('rightcolumn')
-    const box1 = Array.from(col1.getElementsByClassName('box'))
-    const box2 = Array.from(col2.getElementsByClassName('box'))
-
-    const shuflleElems = elem => elem
-      .map(value => ({ value, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value)
-
-    const setOrder = elem => elem.forEach((item, index) => (item.style.order = index))
-
-    setOrder(shuflleElems(box1))
-    setOrder(shuflleElems(box2))
-    // shuffle div end
-
-    const allcon = document.getElementsByClassName('wrp')[0]
-    const header = document.getElementsByTagName('header')[0]
-    const menus = document.getElementsByClassName('exp')
-    // header.addEventListener('click', function (e) {
-    //   e.preventDefault()
-    //   document.getElementsByClassName('classgrid')[0].classList.toggle('animations-togle')
-    //   e.currentTarget.style.animationState = 'paused'
-    //   return false
-    // })
-    function menu (e) {
-      if ((e.target.className !== 'act') && ((menus.length > 0 && e.target.getAttribute('aria-label') !== 'button') || (document.documentElement.offsetWidth > 481))) {
-        document.getElementsByClassName('act').length && document.getElementsByClassName('act')[0].classList.remove('act')
+  $('body').on('click',
+    '.arrowd, #kmenu.collapse.navbar-collapse>ul.nav.navbar-nav>li.dropdown.open>ul.dropdown-menu>li>a, #kmenu.collapse.navbar-collapse>ul.nav.navbar-nav>li>a, .breadcrumb a',
+    function (e) {
+      e.preventDefault()
+      $('#headImage').addClass('pause')
+      let t = $(this).attr('href')
+      let i = 0
+      i = (t = t.substring(1, t.length)).slice(0, -1) === 'collapse' ? 102 : 50
+      const id = '#' + t
+      if (id.length > 1) {
+        $('html, body').animate({
+          scrollTop: $(id).offset().top - i
+        }, 'slow')
       }
-      if (e.target.getAttribute('aria-label') === 'button') {
-        e.preventDefault()
-        e.target.classList.toggle('act')
-        document.getElementsByClassName('wraper')[0].scrollTop = menus[0].parentElement.scrollHeight
-      }
-    }
+    })
 
-    setTimeout(function () {
-      // document.getElementsByClassName('wraper')[0].style.display = 'block'
-      document.getElementById('spiner').style.display = 'none'
-    }, 1000)
-
-    document.addEventListener('click', menu.bind(this))
-    // document.querySelectorAll('a').forEach(e => e.addEventListener('dragstart', e => e.preventDefault()))
-    allcon.addEventListener('scroll', debounce(function () {
-      allcon.scrollTop > 48 && allcon.offsetWidth > 480 ? header.classList.add('zero') : header.classList.remove('zero')
-    }, 60))
-
-    if (document.addEventListener) {
-      document.addEventListener('contextmenu', function (e) {
-        e.preventDefault()
-      }, false)
-    } else {
-      document.attachEvent('oncontextmenu', function () {
-        window.event.returnValue = false
-      })
-    }
+  $('#gotoac').on('click', function (e) {
+    e.preventDefault()
+    $('.navbar-collapse.in').animate({
+      scrollTop: 168
+    }, 'slow')
   })
-}())
+
+  $('#headImage,.arrowd').click(function () {
+    $('#headImage').toggleClass('pause')
+  })
+  $(document).ready(function () {
+    $('#loader').removeClass('loader')
+  })
+})(jQuery)
