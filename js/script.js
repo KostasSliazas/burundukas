902da8df280df09007370ed54fec4079cffb7844
Kostas <kostas.sliazas@gmail.com>
2022-07-30 16:27:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,28 +7,27 @@
     '<a data-toggle="collapse" data-target=".navbar-collapse.in" style="background:#3b5998;color:#eee; border-radius:5px; line-height:50px; margin:0 24px; padding:0 6px;" href="https://www.facebook.com/sharer/sharer.php?u=https://kostassliazas.github.io/burundukas.github.io/" rel="nofollow" target="_blank">Share</a>'
   )
 
-  $('body').on('click',
-    '.arrowd, #kmenu.collapse.navbar-collapse>ul.nav.navbar-nav>li.dropdown.open>ul.dropdown-menu>li>a, #kmenu.collapse.navbar-collapse>ul.nav.navbar-nav>li>a, .breadcrumb a',
-    function (e) {
-      e.preventDefault()
-      $('#headImage').addClass('pause')
-      let t = $(this).attr('href')
-      let i = 0
-      i = (t = t.substring(1, t.length)).slice(0, -1) === 'collapse' ? 102 : 50
-      const id = '#' + t
-      if (id.length > 1) {
-        $('html, body').animate({
-          scrollTop: $(id).offset().top - i
-        }, 'slow')
-      }
-    })
+  // $('body').on('click', '#kmenu', function (e) {
+  //   $('#headImage').addClass('pause')
+  //   let t = $(e.target).attr('href') || ''
+  //   let i = 0
+  //   i = (t = t.substring(1, t.length)).slice(0, -1) === 'collapse' ? 102 : 50
+  //   console.log(t)
+  //   if (t.length > 0) {
+  //     e.preventDefault()
+  //     const id = '#' + t
+  //     $('html, body').animate({
+  //       scrollTop: $(id).offset().top - i
+  //     }, 'slow')
+  //   }
+  // })
 
-  $('#gotoac').on('click', function (e) {
-    e.preventDefault()
-    $('.navbar-collapse.in').animate({
-      scrollTop: 168
-    }, 'slow')
-  })
+  // $('#gotoac').on('click', function (e) {
+  //   e.preventDefault()
+  //   $('.navbar-collapse.in').animate({
+  //     scrollTop: 168
+  //   }, 'slow')
+  // })
 
   $('#headImage,.arrowd').click(function () {
     $('#headImage').toggleClass('pause')
