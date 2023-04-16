3f25572ac4ed29acd0392970cb8080d187ddf68e
Kostas <kostas.sliazas@gmail.com>
2023-04-16 14:47:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,23 @@
-(function ($) {
-  const ext = document.createElement('script')
-  ext.setAttribute('defer', 'defer')
-  ext.setAttribute('src', './js/gall7.min.js')
-  document.head.appendChild(ext)
-  $('#kmenu > ul.nav.navbar-nav.navbar-right').append(
-    '<a data-toggle="collapse" data-target=".navbar-collapse.in" style="background:#3b5998;color:#eee; border-radius:5px; line-height:50px; margin:0 24px; padding:0 6px;" href="https://www.facebook.com/sharer/sharer.php?u=https://kostassliazas.github.io/burundukas.github.io/" rel="nofollow" target="_blank">Share</a>'
-  )
+(function (w,d) {
 
-  // $('body').on('click', '#kmenu', function (e) {
-  //   $('#headImage').addClass('pause')
-  //   let t = $(e.target).attr('href') || ''
-  //   let i = 0
-  //   i = (t = t.substring(1, t.length)).slice(0, -1) === 'collapse' ? 102 : 50
-  //   console.log(t)
-  //   if (t.length > 0) {
-  //     e.preventDefault()
-  //     const id = '#' + t
-  //     $('html, body').animate({
-  //       scrollTop: $(id).offset().top - i
-  //     }, 'slow')
-  //   }
-  // })
+  d.addEventListener('DOMContentLoaded', function(e){
 
-  $('#accordion').on('click', function (e) {
-    e.preventDefault()
-    $(this).removeClass('okty')
+    const ext = d.createElement('script')
+    ext.setAttribute('defer', 'defer')
+    ext.setAttribute('src', './js/gall7.min.js')
+    d.head.appendChild(ext)
+    // d.querySelector('#kmenu > ul.nav.navbar-nav.navbar-right').innerHTML = '<a data-toggle="collapse" data-target=".navbar-collapse.in" style="background:#3b5998;color:#eee; border-radius:5px; line-height:50px; margin:0 24px; padding:0 6px;" href="https://www.facebook.com/sharer/sharer.php?u=https://kostassliazas.github.io/burundukas.github.io/" rel="nofollow" target="_blank">Share</a>'
+    
+
+    let headimage = d.querySelector('#headImage')
+    d.addEventListener('click', function (e) {
+      if(e.target.classList.contains('arrowd') || e.target.id === 'headImage')
+      headimage.classList.toggle('pause')
+    })
+
+    d.querySelector('#loader').classList.remove('loader')
   })
 
-  $('#headImage,.arrowd').click(function () {
-    $('#headImage').toggleClass('pause')
-    $('#accordion').addClass('okty')
-  })
-  $(document).ready(function () {
-    $('#loader').removeClass('loader')
-  })
-})(jQuery)
+})(window,document)
 
 
