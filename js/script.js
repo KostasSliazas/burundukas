a5871d263e6c241fade33271f1ff5a26bbcf66e0
Kostas <kostas.sliazas@gmail.com>
2023-04-25 04:29:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,11 +7,15 @@
     ext.setAttribute('src', './js/gall7.min.js')
     d.head.appendChild(ext)
     // d.querySelector('#kmenu > ul.nav.navbar-nav.navbar-right').innerHTML = '<a data-toggle="collapse" data-target=".navbar-collapse.in" style="background:#3b5998;color:#eee; border-radius:5px; line-height:50px; margin:0 24px; padding:0 6px;" href="https://www.facebook.com/sharer/sharer.php?u=https://kostassliazas.github.io/burundukas.github.io/" rel="nofollow" target="_blank">Share</a>'
-  
+    let langue = d.querySelector('#langue')   
     let headimage = d.querySelector('#headImage')
     d.addEventListener('click', function (e) {
       if(e.target.classList.contains('arrowd') || e.target.id === 'headImage')
       headimage.classList.toggle('pause')
+    })
+
+    langue.addEventListener('click', function (e) {    
+      document.documentElement.lang = document.documentElement.lang === 'lt' ? 'en-US' : 'lt'
     })
 
     d.querySelector('#loader').classList.remove('loader')
